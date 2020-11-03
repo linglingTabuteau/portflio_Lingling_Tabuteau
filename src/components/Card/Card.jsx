51017d520b01bdacc61d2daf672dc9fa8b2620cb
Lingling Tabuteau <linging.tabuteau@gmail.com>
2020-11-03 23:40:28 +0100

    add images to cards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,7 +4,7 @@ import "./Card.css";
 const Card = (props) => {
   return (
     <div class="card Card" style={{ width: "18rem" }}>
-      <img src="..." class="card-img-top" alt="..."></img>
+      <img src={props.img} class="card-img-top" alt="..." />
       <div class="card-body">
         <h5 class="card-title">{props.projectName}</h5>
         <p class="card-text">{props.projectDescription}</p>
@@ -13,6 +13,15 @@ const Card = (props) => {
         </a>
       </div>
     </div>
+
+    // <div class="card" style="width: 18rem;">
+    // <img src="..." class="card-img-top" alt="...">
+    // <div class="card-body">
+    //   <h5 class="card-title">Card title</h5>
+    //   <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    //   <a href="#" class="btn btn-primary">Go somewhere</a>
+    // </div>
+    // </div>
   );
 };
 
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -9,41 +9,49 @@ const Cards = () => {
         projectName="Ghibli Film"
         projectDescription="Ghibli Film Adventure"
         link="https://lingling-tabuteau-movie-list.herokuapp.com/"
+        img="./budget.jpg"
       />
       <Card
         projectName="Game Dice"
         projectDescription="Funny small game Think nothing Just have fun Enjoy your gameˆ-ˆ"
         link="https://game-dice-lingling-tabuteau.herokuapp.com/"
+        img="./budget2.jpg"
       />
       <Card
         projectName="Budget Management"
         projectDescription="simple way to manage your budget"
         link="https://budget-project-ltabuteau.herokuapp.com/"
+        img="./budget2.jpg"
       />
       <Card
         projectName="Site Vitrine ANEKO - ongoning"
         projectDescription="site vitrine for start-up ANEKO"
         link="ongoning"
+        img="./budget2.jpg"
       />
       <Card
         projectName="E-commerce site - ongoning"
         projectDescription="E-commerce site - ongoing"
         link="ongoning"
+        img="./budget.jpg"
       />
       <Card
         projectName="Sharing your experiences"
         projectDescription="Anonymous social network - ongoing"
         link="ongoning"
+        img="./budget.jpg"
       />
       <Card
         projectName="Linkedin Lingling Tabuteau"
         projectDescription="Hope to continue my following adventures with you ˆ-ˆ"
         link="https://www.linkedin.com/in/lingling-tabuteau/"
+        img="./budget.jpg"
       />
       <Card
         projectName="Next Adventure ..."
         projectDescription="Hope to continue my following adventures with you ˆ-ˆ"
         link="ongoning"
+        img="./budget.jpg"
       />
     </div>
   );
